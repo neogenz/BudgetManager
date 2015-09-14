4106a1cee1f306a806f4442013f2dc1d9ebb162a
neogenz <maxime.desogus@gmail.com>
2015-09-14 21:59:05 +0200

    - Refactorisation de l'architecture de l'application et réécriture des contrôleurs. - Mise en place de service - Implémentation de fichier de config - Mise en place de Bean - Changement du formulaire d'inscription

--- server.js
+++ server.js
@@ -1,40 +1,34 @@
-// set up ========================================================================================================
-// get all the tools we need
-var express = require("express");
+var express = require('express');
 var app = express();
 var port = 5000;
-var http = require("http");
+var http = require('http');
 
-var morgan = require("morgan");
-var models = require("./models");
-var bodyParser = require("body-parser");
-var cookieParser = require('cookie-parser');
-var jwt = require("jsonwebtoken");
+var morgan = require('morgan');
+var models = require('./model');
+var bodyParser = require('body-parser');
+//var cookieParser = require('cookie-parser');
+var jwt = require('jsonwebtoken');
+var cors = require('cors');
 
 
 // configuration ==============================================================
 app.set('port', process.env.PORT || port);
-process.env.JWT_SECRET = "applciationbudgetmanager";
+process.env.JWT_SECRET = 'applciationbudgetmanager';
 
-app.use(cookieParser()); // read cookies (needed for auth)
+//app.use(cookieParser()); // read cookies (needed for auth)
 app.use(bodyParser.json());// get information from html forms
 app.use(bodyParser.urlencoded({extended: true}));
 
 // options ====================================================================
 app.use(morgan('dev')); // log every request to the console
-app.use(function (req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, content-type, Authorization');
-    next();
-});
+app.use(cors({credentials: true, origin: true}));
 
 // models init =================================================================
 models.sequelize.sync(/*{force: true}*/).then(function () {
     http.createServer(app).listen(app.get('port'), function () {
         console.log('Express server listening on port ' + app.get('port'));
         require('./routing')(app, models, jwt);
-        require(__dirname + "/dal/ProvisionalPlan")(app, models, jwt);
-        require(__dirname + "/dal/Movement")(app, models, jwt);
+        require(__dirname + '/dal/ProvisionalPlan')(app, models, jwt);
+        require(__dirname + '/dal/Movement')(app, models, jwt);
     });
 });
